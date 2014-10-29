d9870d69522da9bf82c471310f54619043049add
Slayerxoxo <coraline.marie.cm@gmail.com>
2014-10-29 15:42:14 +0100

    refactoring en une seule fonction

--- src/hmm_segmenter.py
+++ src/hmm_segmenter.py
@@ -35,14 +35,16 @@ def main(argv):
     train_set = content_handler(argv[0])
     test_set = content_handler(argv[1])
 
-    model_bigram = train_bigram(train_set.sentences)
-    model_trigram = train_trigram(train_set.sentences)
+    model = train_total(train_set.sentences)
+    #model_bigram = train_bigram(train_set.sentences)
+    #model_trigram = train_trigram(train_set.sentences)
 
     handle = codecs.open(argv[2], 'w', 'utf-8')
     handle.write('<?xml version="1.0" encoding="UTF-8" ?>\n')
     handle.write('<dataset>\n')
     for i in range(len(test_set.raw_sentences)):
-        segmented_sentence = word_segmentation(model_bigram, model_trigram, test_set.raw_sentences[i])
+        segmented_sentence = word_segmentation(model, test_set.raw_sentences[i])
+        #segmented_sentence = word_segmentation(model_bigram, model_trigram, test_set.raw_sentences[i])
         handle.write('\t<sentence sid="'+str(i)+'">\n')
         handle.write('\t\t<raw>'+segmented_sentence+'</raw>\n')
         handle.write('\t</sentence>\n')
@@ -129,99 +131,166 @@ class content_handler(xml.sax.ContentHandler):
 
 
 
+# ################################################################################
+# # Training function bigram (base)
+# ################################################################################
+# def train_bigram(sentences):   # def train(sentences):
+#
+#     observation_prob = {}
+#     state_trans_prob = {}
+#     alphabet_trans_prob = {}
+#     alphabet_type = ["hiragana", "katakana", "romanji", "kanji", "other"]
+#
+#     #Initialisation alphabet probability
+#     for element in alphabet_type:
+#         alphabet_trans_prob[element] = {}
+#         for element2 in alphabet_type:
+#             alphabet_trans_prob[element][element2] = {'c': 0.0, 'b': 0.0}
+#
+#     # Iterates through the sentences
+#     for sentence in sentences:
+#
+#         for i in range(len(sentence)):
+#             sentence[i] = 'c'.join(sentence[i])		# rajoute un c entre chaque caractère non coupé
+#         annotated_sequence = 'b'.join(sentence)		# rajoute un b à la fin de chaque "mot"
+#
+#         previous_state = ''
+#         current_state = ''
+#
+#         for i in range(0, len(annotated_sequence) - 1, 2):
+#             observation = annotated_sequence[i:i + 3]
+#             bigram = observation[0] + observation[2]
+#
+#             current_state = observation[1]
+#
+#             alphabet_trans_prob[get_alphabet(observation[0])][get_alphabet(observation[2])][current_state] += 1
+#
+#             if not observation_prob.has_key(bigram):
+#                 observation_prob[bigram] = {'c': 0.0, 'b': 0.0}
+#             observation_prob[bigram][current_state] += 1.0
+#
+#             if previous_state != '':
+#                 add_one(state_trans_prob, (previous_state, current_state))
+#             previous_state = current_state
+#
+#     # Normalize observation probabilities
+#     for bigram in observation_prob:
+#         norm_factor = observation_prob[bigram]['c'] + observation_prob[bigram]['b']
+#         observation_prob[bigram]['c'] /= norm_factor
+#         observation_prob[bigram]['b'] /= norm_factor
+#
+#     # Normalize transition probabilities
+#     norm_factor = 0.0
+#     for t in state_trans_prob:
+#         norm_factor += state_trans_prob[t]
+#     for t in state_trans_prob:
+#         state_trans_prob[t] /= norm_factor
+#
+#
+#     return [observation_prob, state_trans_prob, alphabet_trans_prob]
+# ################################################################################
+#
+#
+#
+# ################################################################################
+# # Training function trigram + gestion de l'alphabet
+# ################################################################################
+# def train_trigram(sentences):
+#
+#     observation_prob = {}
+#     state_trans_prob = {}
+#     alphabet_trans_prob = {}
+#
+#     alphabet_type = ["hiragana", "katakana", "romanji", "kanji", "other"]
+#     for element in alphabet_type:
+#         alphabet_trans_prob[element] = {}
+#         for element2 in alphabet_type:
+#             alphabet_trans_prob[element][element2] = {'c': 0.0, 'b': 0.0}
+#
+#     # Iterates through the sentences
+#     for sentence in sentences:
+#         annotated_sequence = 'b'.join(sentence)
+#         current_state = ''
+#         previous_state = ''
+#         previous_second_state = ''
+#
+#         for i in range(0, len(annotated_sequence) - 3, 2):
+#             observation = annotated_sequence[i:i + 5]
+#             trigram = observation[0] + observation[2] + observation[4]
+#             current_state = observation[1]
+#
+#             alphabet_trans_prob[get_alphabet(observation[0])][get_alphabet(observation[2])][current_state] += 1
+#
+#             if not observation_prob.has_key(trigram):
+#                 observation_prob[trigram] = {'c': 0.0, 'b': 0.0}
+#             observation_prob[trigram][current_state] += 1.0
+#
+#             if previous_second_state != '':
+#                 add_one(state_trans_prob, (previous_second_state, previous_state, current_state))
+#             elif previous_state != '':
+#                 add_one(state_trans_prob, (previous_state, current_state))
+#             previous_second_state = previous_state
+#             previous_state = current_state
+#
+#     # Normalize observation probabilities
+#     for trigram in observation_prob:
+#         norm_factor = observation_prob[trigram]['c'] + observation_prob[trigram]['b']
+#         observation_prob[trigram]['c'] /= norm_factor
+#         observation_prob[trigram]['b'] /= norm_factor
+#
+#     # Normalize transition probabilities
+#     norm_factor = 0.0
+#     for t in state_trans_prob:
+#         norm_factor += state_trans_prob[t]
+#     for t in state_trans_prob:
+#         state_trans_prob[t] /= norm_factor
+#
+#
+#     return [observation_prob, state_trans_prob, alphabet_trans_prob]
+# ################################################################################
+
+
+
 ################################################################################
-# Training function bigram (base)
+# Training function bigrams + trigrams backoff + alphabets
 ################################################################################
-def train_bigram(sentences):   # def train(sentences):
+def train_total(sentences):
 
     observation_prob = {}
     state_trans_prob = {}
-    alphabet_trans_prob = {}
-    alphabet_type = ["hiragana", "katakana", "romanji", "kanji", "other"]
+    alphabet_prob = {}
 
-    #Initialisation alphabet probability
-    for element in alphabet_type:
-        alphabet_trans_prob[element] = {}
-        for element2 in alphabet_type:
-            alphabet_trans_prob[element][element2] = {'c': 0.0, 'b': 0.0}
+
+    # init alphabet_prob values
+    alphabet_lst = ["hiragana", "katakana", "romanji", "kanji", "other"]
+    for element in alphabet_lst:
+        alphabet_prob[element] = {}
+        for element2 in alphabet_lst:
+            alphabet_prob[element][element2] = {'c': 0.0, 'b': 0.0}
 
     # Iterates through the sentences
     for sentence in sentences:
-
         for i in range(len(sentence)):
-            sentence[i] = 'c'.join(sentence[i])		# rajoute un c entre chaque caractère non coupé
-        annotated_sequence = 'b'.join(sentence)		# rajoute un b à la fin de chaque "mot"
-
-        previous_state = ''
-        current_state = ''
-
-        for i in range(0, len(annotated_sequence) - 1, 2):
-            observation = annotated_sequence[i:i + 3]
-            bigram = observation[0] + observation[2]
-
-            current_state = observation[1]
-
-            alphabet_trans_prob[get_alphabet(observation[0])][get_alphabet(observation[2])][current_state] += 1
-
-            if not observation_prob.has_key(bigram):
-                observation_prob[bigram] = {'c': 0.0, 'b': 0.0}
-            observation_prob[bigram][current_state] += 1.0
-
-            if previous_state != '':
-                add_one(state_trans_prob, (previous_state, current_state))
-            previous_state = current_state
-
-    # Normalize observation probabilities
-    for bigram in observation_prob:
-        norm_factor = observation_prob[bigram]['c'] + observation_prob[bigram]['b']
-        observation_prob[bigram]['c'] /= norm_factor
-        observation_prob[bigram]['b'] /= norm_factor
-
-    # Normalize transition probabilities
-    norm_factor = 0.0
-    for t in state_trans_prob:
-        norm_factor += state_trans_prob[t]
-    for t in state_trans_prob:
-        state_trans_prob[t] /= norm_factor
-
-    
-    return [observation_prob, state_trans_prob, alphabet_trans_prob]
-################################################################################
-
-
-
-################################################################################
-# Training function trigram + gestion de l'alphabet
-################################################################################
-def train_trigram(sentences):
-
-    observation_prob = {}
-    state_trans_prob = {}
-    alphabet_trans_prob = {}
-
-    alphabet_type = ["hiragana", "katakana", "romanji", "kanji", "other"]
-    for element in alphabet_type:
-        alphabet_trans_prob[element] = {}
-        for element2 in alphabet_type:
-            alphabet_trans_prob[element][element2] = {'c': 0.0, 'b': 0.0}
-            
-    # Iterates through the sentences
-    for sentence in sentences:
+            sentence[i] = 'c'.join(sentence[i])
         annotated_sequence = 'b'.join(sentence)
+
         current_state = ''
         previous_state = ''
         previous_second_state = ''
 
-        for i in range(0, len(annotated_sequence) - 3, 2):
-            observation = annotated_sequence[i:i + 5]
+        for i in range(0, len(annotated_sequence)-3, 2):
+            observation = annotated_sequence[i:i+5]
+            bigram = observation[0] + observation[2]
             trigram = observation[0] + observation[2] + observation[4]
             current_state = observation[1]
-
-            alphabet_trans_prob[get_alphabet(observation[0])][get_alphabet(observation[2])][current_state] += 1
+            alphabet_prob[get_alphabet(observation[0])][get_alphabet(observation[2])][current_state] += 1
 
             if not observation_prob.has_key(trigram):
                 observation_prob[trigram] = {'c': 0.0, 'b': 0.0}
             observation_prob[trigram][current_state] += 1.0
+            if not observation_prob.has_key(bigram):
+                observation_prob[bigram] = {'c': 0.0, 'b': 0.0}
+            observation_prob[bigram][current_state] += 1.0
 
             if previous_second_state != '':
                 add_one(state_trans_prob, (previous_second_state, previous_state, current_state))
@@ -231,10 +300,10 @@ def train_trigram(sentences):
             previous_state = current_state
 
     # Normalize observation probabilities
-    for trigram in observation_prob:
-        norm_factor = observation_prob[trigram]['c'] + observation_prob[trigram]['b']
-        observation_prob[trigram]['c'] /= norm_factor
-        observation_prob[trigram]['b'] /= norm_factor
+    for element in observation_prob:
+        norm_factor = observation_prob[element]['c'] + observation_prob[element]['b']
+        observation_prob[element]['c'] /= norm_factor
+        observation_prob[element]['b'] /= norm_factor
 
     # Normalize transition probabilities
     norm_factor = 0.0
@@ -243,44 +312,52 @@ def train_trigram(sentences):
     for t in state_trans_prob:
         state_trans_prob[t] /= norm_factor
 
-    
-    return [observation_prob, state_trans_prob, alphabet_trans_prob]
-################################################################################
+    # norm_factor = 0.0
+    # for i in alphabet_prob.keys():
+    #     for j in alphabet_prob[i].keys():
+    #         for k in alphabet_prob[i][j].keys():
+    #             norm_factor += alphabet_prob[i][j][k]
+    # for i in alphabet_prob.keys():
+    #     for j in alphabet_prob[i].keys():
+    #         for k in alphabet_prob[i][j].keys():
+    #             alphabet_prob[i][j][k] /= norm_factor
 
+    return [observation_prob, state_trans_prob, alphabet_prob]
+################################################################################
 
 
 ################################################################################
 # Function for word segmentation
 ################################################################################
-def word_segmentation(model_bigram, model_trigram, sentence):
+def word_segmentation(model, sentence):
 
-    observation_prob_bigram = model_bigram[0]
-    observation_prob_trigram = model_trigram[0]
-    observation_prob_alphabet = model_bigram[2]
-    state_trans_prob = model_trigram[1]
-
+    observation_prob = model[0]
+    state_trans_prob = model[1]
+    alphabet_prob = model[2]
 
     unseen_prob_b = 0.02
     unseen_prob_c = 0.01
 
     observations = []
-    lattice = [] 
-    
+    lattice = []
 
-    for i in range(len(sentence) - 1):
-        trigram = sentence[i:i + 3]
-        bigram = sentence[i:i + 2]
+
+    for i in range(len(sentence)-1):
+        bigram = sentence[i:i+2]
+        trigram = sentence[i:i+3]
+
         observations.append(trigram)
-        if observation_prob_trigram.has_key(trigram):
-            lattice.append(observation_prob_trigram[trigram])
+
+        if observation_prob.has_key(trigram):
+            lattice.append(observation_prob[trigram])
             lattice[-1]['c'] = max(unseen_prob_c, lattice[-1]['c'])
             lattice[-1]['b'] = max(unseen_prob_b, lattice[-1]['b'])
-        elif observation_prob_bigram.has_key(bigram):
-            lattice.append(observation_prob_bigram[bigram])
+        elif observation_prob.has_key(bigram):
+            lattice.append(observation_prob[bigram])
             lattice[-1]['c'] = max(unseen_prob_c, lattice[-1]['c'])
             lattice[-1]['b'] = max(unseen_prob_b, lattice[-1]['b'])
         else:
-            lattice.append(observation_prob_alphabet[get_alphabet(bigram[0])][get_alphabet(bigram[1])])
+            lattice.append(alphabet_prob[get_alphabet(bigram[0])][get_alphabet(bigram[1])])
             lattice[-1]['c'] = max(unseen_prob_c, lattice[-1]['c'])
             lattice[-1]['b'] = max(unseen_prob_b, lattice[-1]['b'])
 
@@ -289,7 +366,7 @@ def word_segmentation(model_bigram, model_trigram, sentence):
 
     # Initialisation
     V = []
-    bisect.insort(V, (lattice[0]['c'], ['c']))		#insertion automatique au bon endroit
+    bisect.insort(V, (lattice[0]['c'], ['c']))
     bisect.insort(V, (lattice[0]['b'], ['b']))
 
     # Looping the observations
@@ -299,7 +376,7 @@ def word_segmentation(model_bigram, model_trigram, sentence):
         best_V = []
         best_V.append(V[-1])
         highest_prob = best_V[0][0]
-        for j in range(len(V) - 2, -1, -1):
+        for j in range(len(V)-2,-1,-1):
             if V[j][0] >= highest_prob:
                 best_V.append(V[j])
             else:
@@ -307,31 +384,31 @@ def word_segmentation(model_bigram, model_trigram, sentence):
 
         new_V = []
         for prob, stack in best_V:
-
             # Compute probability for continuation
             prob_c = prob * lattice[i]['c'] * state_trans_prob[(stack[-1], 'c')]
-            bisect.insort(new_V, (prob_c, stack + ['c']))
+            bisect.insort(new_V, (prob_c, stack+['c']))
             prob_b = prob * lattice[i]['b'] * state_trans_prob[(stack[-1], 'b')]
-            bisect.insort(new_V, (prob_b, stack + ['b']))
+            bisect.insort(new_V, (prob_b, stack+['b']))
 
         V = list(new_V)
 
     best_V = V[-1][1]
 
-    # Sentence segmentation 
+    # Sentence segmentation
     segmented_sentence = []
-    for i in range(len(sentence) - 2):
+    for i in range(len(sentence)-1):
         if best_V[i] == 'c':
             segmented_sentence.append(sentence[i])
         else:
-            segmented_sentence.append(sentence[i] + " ")
+            segmented_sentence.append(sentence[i]+" ")
     segmented_sentence.append(sentence[-1])
     return ''.join(segmented_sentence)
-################################################################################
 
 
 ################################################################################
-# To launch the script 
+
+################################################################################
+# To launch the script
 ################################################################################
 if __name__ == "__main__":
     main(sys.argv[1:])
